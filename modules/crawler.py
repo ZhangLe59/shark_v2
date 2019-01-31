d46511f9b6ecd32afd5459efe53b01f8a5c7991c
GUO FENG <guofeng20094@gmail.com>
2019-01-31 12:01:10 +0800

    refactor code

--- modules/crawler.py
+++ modules/crawler.py
@@ -13,70 +13,77 @@ from modules.analyseUtility import *
 def get_data_yahoo(shark_db):
     logger = logging.getLogger(__name__)
 
-    stock_list = [
-                  'AMZN', 'LMT', 'BA', 'PDD', 'NFLX', 'FB', 'USNA', 'MDB', 'NIO', 'SQ', 'GDS', 'YRD', 'AMD', 'BABA',
-                  'TSLA', 'PLNT', 'MO', 'VIPS',
-                  '0700.HK', '2318.HK', '2202.HK', '1211.HK', '6060.HK', '1579.HK', '6862.HK', '1810.HK', '3690.HK',
-                  'AU8U.SI', 'D05.SI',
-                  'IAG.AX', 'A2M.AX',
-                  '600030.SS', '002415.SZ'
+    CONSTANT_STOCK_POOL = [
+        'AMZN', 'LMT', 'BA', 'PDD', 'NFLX', 'FB', 'USNA', 'MDB', 'NIO', 'SQ', 'GDS', 'YRD', 'AMD', 'BABA',
+        'TSLA', 'PLNT', 'MO', 'VIPS', 'TCEHY',
+        '0700.HK', '2318.HK', '2202.HK', '1211.HK', '6060.HK', '1579.HK', '6862.HK', '1810.HK', '3690.HK',
+        'AU8U.SI', 'D05.SI',
+        'IAG.AX', 'A2M.AX',
+        '600030.SS', '002415.SZ'
     ]
+
+    stock_list = CONSTANT_STOCK_POOL[:]
 
     result = {}
     data_dict = {}
 
     new_stock = ''
-    for idx, stock in enumerate(stock_list):
-        try:
-            link = "https://finance.yahoo.com/quote/" + stock + "/key-statistics"
-
-            response = requests.get(link)
-
-            the_page = response.content
-
-            pattern = "{\"quoteData\":{" + "\"" + stock + "\":{*(.+?)e}"
-            p = re.compile(pattern)
-
-            try:
-                dict_string = "{\"" + stock + "\":{" + p.search(the_page.decode("utf-8")).group(1) + "e}}"
-            except Exception as e:
-                print(stock + ' encountered error, skip')
-                logger.error(str(e))
-                continue
-
-            new_stock = stock.replace('.', '-')
-            stock_list[idx] = new_stock
-            data_dict[new_stock] = json.loads(dict_string)[stock]
-            data_dict[new_stock]['stock_code'] = new_stock
-
-            soup = BeautifulSoup(the_page, features="html.parser")
-
-            for header in soup.findAll('h2'):
-                if header.text == 'Trading Information':
-                    stock_price_hist_tb = header.next_sibling.find('h3').next_sibling
-                    for row in stock_price_hist_tb.findAll('tr'):
-                        key = row.contents[0].text
-                        _text = row.contents[1].text.rstrip("%").replace(',', '')
-                        try:
-                            value = float(_text)
-                        except Exception as e:
-                            value = 999999999
-                        data_dict[new_stock][key] = value
-                    break
-
-            print(stock + ' loaded')
-
-        except Exception as crawl_exception:
-            data_dict.pop(new_stock)
-            print(str(crawl_exception))
+    for idx, stock in enumerate(CONSTANT_STOCK_POOL):
+        get_technical_data_yahoo(idx, stock, data_dict, stock_list)
 
     result['key'] = datetime.now(pytz.timezone('Singapore')).strftime('%Y-%m-%d')
     result['stock_pool'] = stock_list
     result['data'] = data_dict
 
-    # save_to_mongo_db(stock_collection, result)
-
     save_to_mongo_db(shark_db, 'stocks', result)
     # save_to_local_file(result)
 
     return result
+
+
+def get_technical_data_yahoo(idx, stock, data_dict, stock_list):
+    try:
+        link = "https://finance.yahoo.com/quote/" + stock + "/key-statistics"
+        the_page = requests.get(link).content
+
+        pattern = "{\"quoteData\":{" + "\"" + stock + "\":{*(.+?)e}"
+        p = re.compile(pattern)
+
+        try:
+            dict_string = "{\"" + stock + "\":{" + p.search(the_page.decode("utf-8")).group(1) + "e}}"
+        except Exception as e:
+            print(stock + ' encountered error, skip')
+            logger.error(str(e))
+            return
+
+        new_stock = stock.replace('.', '-')
+        stock_list[idx] = new_stock
+        data_dict[new_stock] = json.loads(dict_string)[stock]
+        data_dict[new_stock]['stock_code'] = new_stock
+
+        soup = BeautifulSoup(the_page, features="html.parser")
+
+        for header in soup.findAll('h2'):
+            if header.text == 'Trading Information':
+                stock_price_hist_tb = header.next_sibling.find('h3').next_sibling
+                for row in stock_price_hist_tb.findAll('tr'):
+                    key = row.contents[0].text
+                    _text = row.contents[1].text.rstrip("%").replace(',', '')
+                    try:
+                        value = float(_text)
+                    except Exception as e:
+                        value = 999999999
+                    data_dict[new_stock][key] = value
+                break
+
+        print(stock + ' loaded')
+
+    except Exception as crawl_exception:
+        data_dict.pop(new_stock)
+        print(str(crawl_exception))
+
+
+def get_earning_dates(idx, stock):
+    # https://finance.yahoo.com/calendar/earnings/?symbol=BABA
+
+    return
